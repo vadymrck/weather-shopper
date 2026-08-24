a1f30ab868d2e2b8a84ce7d3f86b8d9a05ad979e
vm <marochok+git@gmail.com>
2026-08-24 10:37:30 +0200

    ci: prevent focused tests in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   fullyParallel: false,
   workers: 1,
   retries: 0,
+  forbidOnly: !!process.env.CI,
   reporter: [["list"], ["html", { open: "never" }]],
   use: {
     baseURL:
